372d1abd052089f37e0c6f7540bcffb04e808696
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2018-11-17 20:51:37 +0300

    user model added

--- ProfileScreenApp/app/src/main/java/com/example/okanserdaroglu/profilescreenapp/views/MainActivity.java
+++ ProfileScreenApp/app/src/main/java/com/example/okanserdaroglu/profilescreenapp/views/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.okanserdaroglu.profilescreenapp.views;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.example.okanserdaroglu.profilescreenapp.R;
 
 public class MainActivity extends AppCompatActivity {

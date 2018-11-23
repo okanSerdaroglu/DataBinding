c22aeb790c173bb63e34d67c8e8c33d6b5e8c2aa
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2018-11-23 12:55:20 +0300

    activity main binding

--- DataBindingJavaApp/app/src/main/java/com/example/databindingjavaapp/MainActivity.java
+++ DataBindingJavaApp/app/src/main/java/com/example/databindingjavaapp/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.databindingjavaapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
+import com.example.databindingjavaapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding; // this class name generates from layout name
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        Ogrenci ogr1 = new Ogrenci(1,"okan");
+        Ogrenci ogr2 = new Ogrenci(2, "Büşra");
+
+        /*mainBinding.ogrNO.setText(String.valueOf(ogr1.getNo()));
+        mainBinding.tvIsim.setText(ogr1.getIsim());*/
+
+        mainBinding.setOgrenci(ogr2);
+
     }
 }

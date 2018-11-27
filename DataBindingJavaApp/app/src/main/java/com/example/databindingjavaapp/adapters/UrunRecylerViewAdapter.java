9fd56c13717e9a00c2f2e950ab33b88cef3fb5e9
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2018-11-27 22:34:24 +0300

    recylerView adapter with dataBinding

--- DataBindingJavaApp/app/src/main/java/com/example/databindingjavaapp/adapters/UrunRecylerViewAdapter.java
+++ DataBindingJavaApp/app/src/main/java/com/example/databindingjavaapp/adapters/UrunRecylerViewAdapter.java
@@ -1,20 +1,17 @@
 package com.example.databindingjavaapp.adapters;
-
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.example.databindingjavaapp.R;
+import com.example.databindingjavaapp.databinding.TekSutunUrunLayoutBinding;
 import com.example.databindingjavaapp.models.Urun;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class UrunRecylerViewAdapter extends RecyclerView.Adapter<UrunRecylerViewAdapter.ViewHolder> {
@@ -23,22 +20,31 @@ public class UrunRecylerViewAdapter extends RecyclerView.Adapter<UrunRecylerView
     private List<Urun> tumUrunler = new ArrayList<>();
     private Context context;
 
-    public UrunRecylerViewAdapter (Context context,List<Urun>tumUrunler){
+    public UrunRecylerViewAdapter (Context context, List<Urun>tumUrunler){
         this.context = context;
         this.tumUrunler = tumUrunler;
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.tek_sutun_urun_layout,parent,false);
-        return new ViewHolder(view);
+    public UrunRecylerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //View view = LayoutInflater.from(context).inflate(R.layout.tek_sutun_urun_layout,parent,false);
+        //return new ViewHolder(view);
+
+        TekSutunUrunLayoutBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context),
+                        R.layout.tek_sutun_urun_layout,parent,false);
+        return new ViewHolder(binding.getRoot());
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull UrunRecylerViewAdapter.ViewHolder holder, int position) {
 
-        holder.setData (tumUrunler.get(position), position);
+        //holder.setData(tumUrunler.get(position),position);
+        Urun urun = tumUrunler.get(position);
+        holder.binding.setUrun(urun);
+        holder.binding.executePendingBindings(); // bir sonraki satırı beklemeden onunla ilgili bindingleri yapmak için bu metod
 
     }
 
@@ -47,26 +53,30 @@ public class UrunRecylerViewAdapter extends RecyclerView.Adapter<UrunRecylerView
         return tumUrunler.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        ConstraintLayout tumLayout;
-        TextView baslik;
-        TextView fiyat;
+        //ConstraintLayout tumLayout;
+        //TextView baslik;
+        //TextView fiyat;
+
+        TekSutunUrunLayoutBinding binding;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            tumLayout = itemView.findViewById(R.id.tumLayout);
-            baslik = itemView.findViewById(R.id.tvUrunBaslik);
-            fiyat = itemView.findViewById(R.id.tvFiyat);
+            binding = DataBindingUtil.bind(itemView); // verilen datalar itemView a bağlanır
+
+            //tumLayout = itemView.findViewById(R.id.tumLayout);
+            //baslik = itemView.findViewById(R.id.tvUrunBaslik);
+            //fiyat = itemView.findViewById(R.id.tvFiyat);
+        }
+
+        public void setData (Urun urun,int position){
+
+            //baslik.setText(urun.getBaslik());
+            //fiyat.setText(""+urun.getFiyat());
 
         }
 
-        public void setData(Urun urun, int position) {
-
-            baslik.setText(urun.getBaslik());
-            fiyat.setText(""+urun.getFiyat());
-
-        }
     }
 }
--- DataBindingJavaApp/app/src/main/java/com/example/databindingjavaapp/fragments/MainFragment.java
+++ DataBindingJavaApp/app/src/main/java/com/example/databindingjavaapp/fragments/MainFragment.java
@@ -1,15 +1,23 @@
 package com.example.databindingjavaapp.fragments;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.databindingjavaapp.adapters.UrunRecylerViewAdapter;
 import com.example.databindingjavaapp.databinding.FragmentMainBinding;
+import com.example.databindingjavaapp.models.Urun;
+import com.example.databindingjavaapp.utils.Urunler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
@@ -25,14 +33,34 @@ public class MainFragment extends Fragment implements SwipeRefreshLayout.OnRefre
 
         fragmentMainBinding.swipeRefreshLayout.setOnRefreshListener(this);
 
+        setUrunlerRecylerView ();
+
         return fragmentMainBinding.getRoot();
 
     }
 
+
     @Override
     public void onRefresh() {
 
+    }
+
+    private void setUrunlerRecylerView() {
+
+
+        GridLayoutManager manager = new GridLayoutManager(getActivity(),2);
+        fragmentMainBinding.tumUrunlerRecylerView.setLayoutManager(manager);
+
+        Urunler urunler = new Urunler();
+        List<Urun>tumUrunler = new ArrayList<>();
+        tumUrunler.addAll(Arrays.asList(urunler.tumUrunlerDizi));
+
+        UrunRecylerViewAdapter urunRecylerViewAdapter = new UrunRecylerViewAdapter(getActivity(),tumUrunler);
+        fragmentMainBinding.tumUrunlerRecylerView.setAdapter(urunRecylerViewAdapter);
 
 
     }
+
+
+
 }
